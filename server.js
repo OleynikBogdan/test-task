448907595382f7548a67d2e4035149334c9b1d71
saburoDARAME <profenyx@gmail.com>
2024-06-15 19:57:11 +0300

    change another html location

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app= express();
 
-app.use(express.static('./dist/test-task'));
+app.use(express.static('./dist/test-task/browser'));
 
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {root: 'dist/test-task/browser'})
