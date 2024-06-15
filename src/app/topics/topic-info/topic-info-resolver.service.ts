9d0c5d4780ecbc8c2288f2cf820de3b5c7676005
saburoDARAME <profenyx@gmail.com>
2024-06-15 15:02:06 +0300

    create card, info, pipes, resolver, use free API

---
+++ src/app/topics/topic-info/topic-info-resolver.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { map, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { selectTopicsSelector } from '../../store/search.selectors';
+import { ITopic } from '../../store/search.models';
+
+@Injectable()
+export class TopicInfoResolverService implements Resolve<ITopic> {
+
+    searchedTopics$ = this.store.select(selectTopicsSelector);
+
+  constructor(
+    private store: Store,
+    private router: Router,
+  ) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<any> {
+
+    return this.searchedTopics$.pipe(
+        tap(topics => {
+          if (!topics) {
+            this.router.navigateByUrl('/')
+          }
+        }),
+        map(topics => topics?.filter(topic => topic.id === +route.params['id'])[0]));
+  }
+}
